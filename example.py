aa4a65fe986691b9ca148bbe34f95fab1709ce87
Yinghui Wang <wyh804@gmail.com>
2020-08-12 19:12:16 -0400

    Update example.py

--- example.py
+++ example.py
@@ -19,6 +19,6 @@ ObjectDetector = ObjectDetection()
 
 for _ in range(100):
     # for each data piece, pass image + lidar data to path extraction and object detection functions
-    sensorData = [] # 
+    sensorData = [] # lidar or camera
     traj3d = PathExtractor.process(sensorData)
     object3d = ObjectDetector.process(sensorData)
