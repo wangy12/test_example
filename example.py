ecbcd1f8201c26f146e8a37b97a9f4cf7c0f179a
Yinghui WANG <T0210240@ta1.tas-can.com>
2020-08-12 18:57:14 -0400

    add an example

---
+++ example.py
@@ -0,0 +1,24 @@
+Class PathExtraction3D():
+	def __init__(self):
+		pass
+		
+	def process(self, **kwargs):
+		return traj3D
+		
+Class ObjectDetection():
+	def __init__(self):
+		pass
+		
+	def process(self, **kwargs):
+		return object3D
+		
+import PathExtraction3D, ObjectDetection
+
+PathExtractor = PathExtraction3D()
+ObjectDetector = ObjectDetection()
+
+for _ in range(100):
+    # for each data piece, pass image + lidar data to path extraction and object detection functions
+    sensorData = []
+    traj3d = PathExtractor.process(sensorData)
+	object3d = ObjectDetector.process(sensorData)
